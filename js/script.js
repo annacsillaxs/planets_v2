0b94f199693dde8faa07fd19b0244d3f57260e05
annacsillaxs <anna.seregi@gmail.com>
2021-07-27 19:44:45 +0100

    modified setThemeColor()

--- js/script.js
+++ js/script.js
@@ -187,7 +187,7 @@ function setThemeColor(data, idx) {
       btn.style.borderBottomColor = color.theme;
     } else {
       btn.style.backgroundColor = 'transparent';
-      btn.style.borderBottomColor = 'transparent';
+      btn.style.borderBottomColor = 'inherit';
     }
   })
 }
